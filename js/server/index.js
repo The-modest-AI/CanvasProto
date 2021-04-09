c7f9c0a91cc87ee5de8ddcb8c49702cb5070669e
The-modest-AI <ankanghosh977@gmail.com>
2021-04-09 22:44:17 +0530

    Added room functionality

--- js/server/index.js
+++ js/server/index.js
@@ -3,34 +3,70 @@ const app = express();
 const path = require(`path`);
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const formattedMessage = require(`../../public/utils/message`);
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+} = require(`../../public/utils/users`);
+
+const aiName = `Bot`;
 
 app.get(`/join`, (req, res) => {
     res.sendFile(path.join(__dirname, `../../public/enter.html`));
 });
 
 app.get(`/index.html`, (req, res) => {
-    console.log(req.query.username);
-    console.log(req.query.code);
     res.sendFile(path.join(__dirname, `../../public/index.html`));
 });
 
 app.use(express.static(path.join(__dirname, "../../public/")));
 
-io.on('connection', (socket) => {
-    console.log('A User connected');
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
+io.on('connection', socket => {
+    socket.on(`join-room`, ({username, code}) => {
+        const user = userJoin(socket.id, username, code);
+        //join
+        socket.join(user.code);
+        //welcome message
+        socket.emit(`message`, formattedMessage(aiName, `Welcome to the game`));
+        //say to all users in specific room
+        socket.broadcast.to(user.code).emit(`message`, formattedMessage(aiName, `${user.username} joined the game`));
+        //Send info for sidebar
+        io.to(user.code).emit('roomUsers', {users: getRoomUsers(user.code)});
     });
+
+    //Custom User messages
+    socket.on('chat-message', msg => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.code).emit('message', formattedMessage(user.username, msg));
+    });
+
+    //On Disconnect
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        //check if any user exists
+        if (user) {
+            io.to(user.code).emit('message', formattedMessage(aiName, `${user.username} has left the game`));
+        }
+
+        //Send info for sidebar
+        io.to(user.code).emit('roomUsers', {users: getRoomUsers(user.code)});
+    });
+
+    //clear canvas event
     socket.on(`clear-event`, () => {
         socket.broadcast.emit(`clear-event`);
-        console.log(`clear`);
-        socket.on('coordinates-sent', (metaData) => {
-            socket.broadcast.emit(`draw coordinates`, metaData);
-            console.log(JSON.stringify(metaData, null, 2).replace(/['"]+/g, ''));
-        });
+    });
+
+    //draw canvas event
+    socket.on('coordinates-sent', (metaData) => {
+        socket.broadcast.emit(`draw coordinates`, metaData);
     });
 });
 
-http.listen(3000, () => {
-    console.log("Connected the server... Ready for stuff...")
+const PORT = 3000;
+http.listen(PORT, () => {
+    console.log(`Server Online at PORT ${PORT}`);
 });
